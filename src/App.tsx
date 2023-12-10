f27055185a7aba70045d10264304c3c5a75b26a4
Mahammad2003 <mahammadasadli2003@gmail.com>
2023-12-11 01:57:05 +0400

    movie-search

--- src/App.tsx
+++ src/App.tsx
@@ -1,136 +1,109 @@
-import React from "react";
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View,} from "react-native";
-
-function App(){
-  return(
-    <SafeAreaView style={styles.area}>
-     <ScrollView  style={{flex:1}}>
-       <View style={styles.header}>
-        <Image source={require('./assets/icons/Menu.png')}/>
-       <View style={{flexDirection:'row',alignItems:'center'}}>
-         <Image source={require('./assets/icons/Search.png')} style={{marginRight:24}}/>
-       <Image source={require('./assets/icons/Shopping.png')}/>
-       </View>
-       </View>
-       <View style={styles.topics}>
-        <Text style={styles.topicsText}>DELICIOUS FOOD</Text>
-        <Text style={styles.topicsText2}>AT YOUR DOOR STEPS</Text>
-       </View>
-        <View style={styles.icon}>
-         <Image source={require('./assets/icons/Allfood.png')} style={{borderWidth:5, marginTop:47, marginLeft:20}}/>
-         <Image source={require('./assets/icons/Rice.png')} style={{marginTop:47}}/>
-         <Image source={require('./assets/icons/fast.png/')} style={{marginTop:47}}/>
-         <Image source={require('./assets/icons/Drinks.png')} style={{marginTop:47, marginLeft:47}}/>
-        </View>
-         <View style={styles.image}>
-          <Image source={require('./assets/images/Rice.png')}/>
-          <Image style={{marginLeft:20}} source={require('./assets/images/Zinger.png')}/>
-         </View>
-        <View style={styles.Text}>
-         <Text style={styles.Text1}>Popular</Text>
-         <Text style={styles.Text2}>Now</Text>
-        </View>
-        <View style={styles.footer}>
-          <Image style={{marginHorizontal:8,marginVertical:5}} source={require('./assets/images/Rectangle.png')}/>
-          <View style={styles.footerText}>
-          <Text style={styles.footerText1}>Chicken Manchurian</Text>
-          <Text>Chinese Cuisine</Text>
-          <View style={styles.footicon1}>
-          <Text style={styles.footText2}>$110.50</Text>
-          <Image style={styles.footicon} source={require('./assets/icons/Vector.png')}/>
-          </View>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
+import React, {useState} from 'react';
+import {StyleSheet,ScrollView,TextInput,Image,View,TouchableOpacity,Text,} from 'react-native';
+import MovieCard from './Moviecard'
+interface Movie {
+  show: {
+    id: number;
+    name: string;
+    summary: string;
+    image: {
+      medium: string;
+      original: string;
+    };
+    rating: {
+      average: number;
+      text:string
+    };
+  };
 }
-export default App
- 
+
+const API_URL = 'http://api.tvmaze.com/search/shows?q=';
+
+const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchValue, setSearchValue] = useState('setSearchValue');
+
+  const onChangeText =(text:any)=> {
+    setSearchValue(text);
+  };
+
+  const handleSearch = async () => {
+    const responce = await fetch(API_URL + searchValue);
+    const result = await responce.json();
+    setMovies(result);
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.main}>
+       <TextInput style={styles.search} onChangeText={onChangeText} />
+        <TouchableOpacity style={styles.button} onPress={handleSearch}>
+          <Image source={require('./assets/icons/Search.png')} />
+        </TouchableOpacity>
+      </View>
+      {movies.map((item, index) => {
+        <Text style={{fontSize:14}}></Text>
+        return (
+          <MovieCard 
+            data={item?.show?.summary}
+            key={index}
+            name={item?.show?.name}
+            imageUrl={item?.show?.image?.medium}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+};
+export default App;
+
 const styles = StyleSheet.create({
-  area:{
-    flex:1
+  container: {
+    flexGrow: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
-  header:{
-    flexDirection:'row',
-    margin:30,
-    marginLeft:20,
+  main: {
+    flexDirection: 'row',
+    backgroundColor: 'gray',
+    width: '100%',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: 'black',
+  },
+  search: {
+    backgroundColor: '#d3d3d3',
+    width: '75%',
+    marginVertical: 10,
+    borderRadius: 12,
+    marginTop: 40,
+    marginLeft: 15,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#d3d3d3',
+    borderRadius: 18,
+    height: 50,
+    width: "15%",
+    marginLeft: 10,
+    marginTop: 40,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
-  topics:{
-      marginTop:10,
-      marginLeft:26,
+  text1: {
+    fontSize: 25,
+    color: 'black',
+    marginLeft: 5,
+    marginTop: 10,
   },
-  topicsText:{
-    fontSize: 32,
-    color:'black',
-    fontWeight:'bold'
-  },
-  topicsText2:{
-    fontSize:20,
-    color:'#333333',
-    fontWeight:'500' 
-  },
-  icon:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    marginLeft:10,
-    margin:10
-  },
-  image:{
-    flexDirection:'row',
-    justifyContent:'center',
-    marginTop:20,
-    borderRadius:24,
-    marginLeft:3,
-  },
-  Text1:{
-    fontSize:25,
-    fontWeight:'bold',
-    color:'black',
-    marginRight:10
-  },
-  Text2:{
-    fontSize:25
-  },
-  Text:{
-    flexDirection:'row',
-    alignItems:'center',
-    marginLeft:18,
-    marginTop:27
-  },
-  footer:{
-    marginTop:27,
-    display: "flex",
-    flexDirection:'row',
-    marginHorizontal:17,
-    borderWidth:3,
-    borderColor:'black',
-    borderRadius:12
-  },
-  footerText:{
-    marginLeft:16
-  },
-  footerText1:{
-    fontSize:18,
-    color:'black',
-    fontWeight:'bold'
-  },
-  footText2:{
-    marginTop:14,
-    fontSize:20,
-    color:'black',
-    fontWeight:'600'
-  },
-  footicon:{
-    backgroundColor:'#4E5156',
-    borderRadius:8,
-    padding:15,
-    marginLeft:118,
-    marginBottom:24
-  },
-  footicon1:{
-    flexDirection:'row'
-  }
-})
+  text2: {
+    fontSize: 18,
+    color: 'gray',
+    marginLeft: 5,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+});
---
+++ src/Moviecard.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
+
+interface IMovieCard {
+  name: string;
+  imageUrl: string;
+   data:string;
+}
+
+const MovieCard = ({name, imageUrl, data}: IMovieCard) => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Text style={styles.text1}>{name}</Text>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            marginLeft: 5,
+            marginBottom: 45,
+            marginTop: 5,
+            borderRadius: 5,
+          }}
+          source={{uri: imageUrl}}
+        />
+        <Text style={styles.text2}>{data}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default MovieCard;
+
+const styles = StyleSheet.create({
+  text1: {
+    fontSize: 25,
+    color: 'black',
+    marginLeft: 5,
+    marginTop: 10,
+  },
+  text2: {
+    fontSize: 18,
+    color: 'gray',
+    marginLeft: 5,
+    marginTop: 10,
+    marginBottom: 20,
+   },
+});
